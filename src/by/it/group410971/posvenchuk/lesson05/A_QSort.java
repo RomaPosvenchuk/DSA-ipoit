d28aa07aca247fd821bffbecec66c2f12d611c6f
RomaPosvenchuk <posvenchukroman@gmail.com>
2025-06-12 00:17:39 +0300

    Lesson05_A

--- src/by/it/group410971/posvenchuk/lesson05/A_QSort.java
+++ src/by/it/group410971/posvenchuk/lesson05/A_QSort.java
@@ -2,6 +2,7 @@ package by.it.group410971.posvenchuk.lesson05;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -45,33 +46,60 @@ public class A_QSort {
         }
     }
 
-    int[] getAccessory(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
+    int[] getAccessory(InputStream stream) {
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        //число отрезков отсортированного массива
         int n = scanner.nextInt();
         Segment[] segments = new Segment[n];
-        //число точек
         int m = scanner.nextInt();
         int[] points = new int[m];
         int[] result = new int[m];
 
-        //читаем сами отрезки
         for (int i = 0; i < n; i++) {
-            //читаем начало и конец каждого отрезка
-            segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+            int start = scanner.nextInt();
+            int stop = scanner.nextInt();
+            if (start > stop) {
+                int temp = start;
+                start = stop;
+                stop = temp;
+            }
+            segments[i] = new Segment(start, stop);
         }
-        //читаем точки
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
         }
-        //тут реализуйте логику задачи с применением быстрой сортировки
-        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = segments[i].start;
+            ends[i] = segments[i].stop;
+        }
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        for (int i = 0; i < m; i++) {
+            int p = points[i];
+            int count1 = firstGreaterOrEqual(starts, p + 1);
+            int count2 = firstGreaterOrEqual(ends, p);
+            result[i] = count1 - count2;
+        }
+
         return result;
+    }
+
+    private int firstGreaterOrEqual(int[] array, int key) {
+        int low = 0;
+        int high = array.length;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (array[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
     }
 
     //отрезок
