8c09b8662af93472387918e4ab5a06fa1fdec74a
RomaPosvenchuk <posvenchukroman@gmail.com>
2025-06-12 00:10:01 +0300

    Lesson04_B

--- src/by/it/group410971/posvenchuk/lesson04/B_MergeSort.java
+++ src/by/it/group410971/posvenchuk/lesson04/B_MergeSort.java
@@ -31,26 +31,74 @@ public class B_MergeSort {
         }
     }
 
-    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
+    int[] getMergeSort(InputStream stream) {
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-
-        //размер массива
         int n = scanner.nextInt();
-        //сам массив
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
-            System.out.println(a[i]);
         }
 
-        // тут ваше решение (реализуйте сортировку слиянием)
-        // https://ru.wikipedia.org/wiki/Сортировка_слиянием
+        // Вызов сортировки слиянием
+        mergeSort(a, 0, a.length - 1);
 
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return a;
+    }
+
+    private void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            // Рекурсивная сортировка левой и правой половин
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            // Слияние двух отсортированных половин
+            merge(array, left, mid, right);
+        }
+    }
+
+    private void merge(int[] array, int left, int mid, int right) {
+        // Вычисляем размеры временных массивов
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Создаем временные массивы
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Копируем данные во временные массивы
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = array[mid + 1 + j];
+        }
+
+        // Слияние временных массивов
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Копирование оставшихся элементов из leftArray
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Копирование оставшихся элементов из rightArray
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
     }
 
 
